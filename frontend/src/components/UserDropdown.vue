1f35e0988adfa28132b4c01657bd48d1ca15cef7
1337it <91686534+1337it@users.noreply.github.com>
2024-10-03 00:47:34 +0400

    Add files via upload

--- frontend/src/components/UserDropdown.vue
+++ frontend/src/components/UserDropdown.vue
@@ -1,75 +1,83 @@
 <template>
-  <Dropdown :options="dropdownItems">
-    <template v-slot="{ open }">
-      <button
-        class="flex w-[15rem] items-center rounded-md px-2 py-2 text-left"
-        :class="open ? 'bg-white shadow-sm' : 'hover:bg-gray-200'"
-      >
-        <GameplanLogo class="w-8 h-8 rounded" />
-        <div class="ml-2 flex flex-col">
-          <div class="text-base font-medium text-gray-900 leading-none">
-            Gameplan
-          </div>
-          <div class="mt-1 hidden text-sm text-gray-700 sm:inline leading-none">
-            {{ $user().full_name }}
-          </div>
-        </div>
-        <LucideChevronDown class="ml-auto hidden h-4 w-4 sm:inline" />
-      </button>
-    </template>
-  </Dropdown>
-</template>
-<script>
-import { h } from 'vue'
-import { Dropdown } from 'frappe-ui'
-import { showSettingsDialog } from '@/components/Settings/SettingsDialog.vue'
-import LucideCreditCard from '~icons/lucide/credit-card'
-import GameplanLogo from './GameplanLogo.vue'
+	<Dropdown :options="userDropdownOptions" class="p-2">
+		<template v-slot="{ open }">
+			<button class="flex h-12 py-2 items-center rounded-md duration-300 ease-in-out" :class="isCollapsed
+					? 'px-0 w-auto'
+					: open
+						? 'bg-white shadow-sm px-2 w-52'
+						: 'hover:bg-gray-200 px-2 w-52'
+				">
+				<div class="w-8 h-8 rounded-full flex items-center justify-center">
 
-export default {
-  name: 'UserDropdown',
-  components: {
-    Dropdown,
-    GameplanLogo,
-  },
-  computed: {
-    dropdownItems() {
-      return [
-        {
-          icon: 'user',
-          label: 'My Profile',
-          route: {
-            name: 'PersonProfile',
-            params: { personId: this.$user().user_profile },
-          },
-        },
-        {
-          icon: 'settings',
-          label: 'Settings & Members',
-          onClick: () => showSettingsDialog(),
-          condition: () => this.$user().isNotGuest,
-        },
-        {
-          icon: () => h(LucideCreditCard),
-          label: 'Subscription',
-          condition: () =>
-            this.$user().isNotGuest &&
-            window.frappecloud_host &&
-            window.site_name,
-          onClick: () => {
-            window.open(
-              `${window.frappecloud_host}/dashboard/subscription/${window.site_name}`,
-              '_blank',
-            )
-          },
-        },
-        {
-          icon: 'log-out',
-          label: 'Log out',
-          onClick: () => this.$session.logout.submit(),
-        },
-      ]
-    },
-  },
-}
+
+					<Avatar v-if="educationSettings?.logo" :image="educationSettings?.logo" shape="circle" size="xl" />
+					<School v-else />
+				</div>
+				<div class="flex flex-1 flex-col text-left duration-300 ease-in-out" :class="isCollapsed
+						? 'opacity-0 ml-0 w-0 overflow-hidden'
+						: 'opacity-100 ml-2 w-auto'
+					">
+					<div class="text-base font-medium text-gray-900 leading-none">
+						{{ educationSettings?.name || 'Education' }}
+					</div>
+					<div class="mt-1 text-sm text-gray-700 leading-none">
+						{{ user.data.full_name }}
+					</div>
+				</div>
+
+				<div class="duration-300 ease-in-out" :class="isCollapsed
+						? 'opacity-0 ml-0 w-0 overflow-hidden'
+						: 'opacity-100 ml-2 w-auto'
+					">
+					<FeatherIcon name="chevron-down" class="h-4 w-4 text-gray-600" aria-hidden="true" />
+				</div>
+			</button>
+		</template>
+	</Dropdown>
+
+	<ProfileModal />
+</template>
+  
+<script setup>
+
+import { Dropdown, FeatherIcon, Avatar } from 'frappe-ui'
+import { sessionStore } from '@/stores/session'
+import { usersStore } from '@/stores/user';
+import { provide, ref } from 'vue'
+import ProfileModal from '@/components/ProfileModal.vue'
+import { School } from 'lucide-vue-next';
+
+const { user } = usersStore()
+const { logout } = sessionStore()
+defineOptions({
+  inheritAttrs: false
+})
+const props = defineProps({
+	isCollapsed: {
+		type: Boolean,
+		default: false,
+	},
+	educationSettings: {
+	}
+})
+
+
+const showProfileDialog = ref(false)
+provide('showProfileDialog', showProfileDialog)
+
+
+const userDropdownOptions = [
+	{
+		icon: 'user',
+		label: 'Profile',
+		onClick: () => showProfileDialog.value = true,
+	},
+	{
+		icon: 'log-out',
+		label: 'Log out',
+		onClick: () => logout.submit(),
+	},
+]
+
 </script>
+  
