531915928a5c107f4d9775a4c64025ca448c74f1
1337it <91686534+1337it@users.noreply.github.com>
2024-10-03 16:30:17 +0400

    Update MobileLayout.vue

--- frontend/src/components/MobileLayout.vue
+++ frontend/src/components/MobileLayout.vue
@@ -3,23 +3,6 @@
     <div class="h-full overflow-auto" id="scrollContainer">
       <slot />
     </div>
-    <div
-      class="grid grid-cols-5 border-t border-gray-300 standalone:pb-4"
-      :style="{ gridTemplateColumns: `repeat(${tabs.length}, minmax(0, 1fr))` }"
-    >
-      <button
-        v-for="tab in tabs"
-        :key="tab.name"
-        class="flex flex-col items-center justify-center py-3 transition active:scale-95"
-        @click="onTabClick(tab)"
-      >
-        <component
-          :is="tab.icon"
-          class="h-6 w-6"
-          :class="[tab.isActive ? 'text-gray-900' : 'text-gray-600']"
-        />
-      </button>
-    </div>
   </div>
 </template>
 <script>
